cd492f9533281b1ea5d961f73b1503d5a1773f1f
Vishwa Patel <vishwa.patel@medibank.com.au>
2023-08-04 12:27:03 +1000

    Added lotti dependency.

--- Package.swift
+++ Package.swift
@@ -13,8 +13,7 @@ let package = Package(
             targets: ["MyTestPackageRemote", "MyTestFramework"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.2.0")
     ],
     targets: [
         .target(
