df4a0f296568324abd226397191ca7cc77e75cd5
Vishwa Patel <vishwa.patel@medibank.com.au>
2023-08-04 12:37:04 +1000

    Updated the dependency,

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MyTestPackageRemote",
-            targets: ["MyTestPackageRemote", "MyTestFramework"]),
+            targets: ["MyTestPackageRemote"]),
     ],
     dependencies: [
         .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.2.0")
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .target(
             name: "MyTestPackageRemote",
-            dependencies: []),
+            dependencies: ["MyTestFramework", .product(name: "Lottie", package: "lottie-spm")]),
         .binaryTarget(name: "MyTestFramework",
                       url: "https://github.com/vishwa8816/MyTestFrameworkA/releases/download/Frist/MyTestFramework.xcframework.zip",
                       checksum: "806906cda7277317b269b49e306dfb5bc520d6f3b6afc66fea9ac6dc9e47c1ac")
